165a4415a22eb8b671330ba9ab3b4d02e90f65ae
GPR SOLAR INSTALADOR <gabryelrodrigues920@gmail.com>
2025-06-14 11:39:40 -0300

    Adição do aviso e da zamiocuca

--- script.js
+++ script.js
@@ -23,3 +23,17 @@
         showImage(index);
     });
 */
+
+// Efeito de destaque ao passar o mouse sobre a faixa
+const banner = document.querySelector('.frete-banner');
+const texto = document.getElementById('frete-msg');
+
+banner.addEventListener('mouseenter', () => {
+    texto.style.animationPlayState = 'paused';
+    banner.style.backgroundColor = '#337A5B';
+});
+
+banner.addEventListener('mouseleave', () => {
+    texto.style.animationPlayState = 'running';
+    banner.style.backgroundColor = '#285A43';
+});
